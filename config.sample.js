4f5332840134030d5c195d3cd76c68e4b356c51e
Matt Ihlenfield <mtihlenfield@protonmail.com>
2017-04-10 16:41:09 -0400

    Added sample config

---
+++ config.sample.js
@@ -0,0 +1,27 @@
+var config = {
+
+
+	development: {
+
+		mongodb: {
+			"host": "mongodb://localhost:27017/impulse",
+			"username": "",
+			"password": ""
+		},
+
+		session: {
+			secret: "super secret session secret",
+			resave: false,
+			saveUninitialized: false,
+			httpOnly: true,
+		}
+
+
+	}
+
+
+};
+
+exports.get = function get(env) {
+	return config[env] || config.development;
+};
