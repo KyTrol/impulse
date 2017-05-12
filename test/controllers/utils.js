444ae487470cbee80e5d1d1483d31c485f1bfd70
Matt Ihlenfield <mtihlenfield@protonmail.com>
2017-05-12 09:43:38 -0400

    Added unit tests for config.js

---
+++ test/controllers/utils.js
@@ -0,0 +1,14 @@
+var utils = require("../controllers/utils.js");
+var should = require('chai').should();
+
+describe("Testing controllers/utils.js", function() {
+
+  describe("#sendInternalServerError", function() {
+
+  });
+
+  describe("#sendBadRequest", function() {
+
+  });
+
+});
